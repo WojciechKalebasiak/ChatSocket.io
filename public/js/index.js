f6ff5d71acb8f93a8ba46dce5dbed4ce14e3cd46
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-09-12 21:26:37 +0200

    Add basic UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
 const socket = io();
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage',
+        {
+            from: 'User',
+            text: $('[name=message]').val()
+        },
+        function (e) {
+
+        });
+
+});
 socket.on('newMessage', function (data) {
-    console.log(`From: ${data.from}`);
-    console.log(data.text);
+    var li = $('<li>');
+    li.text(`${data.from}: ${data.text}`);
+    $('#messages').append(li);
 });
