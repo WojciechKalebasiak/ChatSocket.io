6e53f1a843ad125009f5ebad327bc6a77d7567bf
Wojciech Kałębasiak <kalebasiakw@gmail.com>
2018-09-12 22:58:26 +0200

    Add styles

--- public/js/index.js
+++ public/js/index.js
@@ -18,9 +18,10 @@ socket.on('newMessage', function (data) {
     $('#messages').append(li);
 });
 socket.on('newLocationMessage', function (data) {
+    var locationButton = $('#send-location');
     var li = $('<li>');
     var a = $('<a>');
-    
+
     a.text('Click here to get my location!');
     a.attr('href', `${data.url}`);
     a.attr('target', '_blank');
@@ -35,9 +36,14 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
+    locationButton.attr('disabled', 'disabled');
+    locationButton.text('Sending location...');
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled');
+        locationButton.text('Send location');
         socket.emit('createLocationMessage', { latitude: position.coords.latitude, longitude: position.coords.longitude });
     }, function () {
+        locationButton.removeAttr('disabled');
         alert('Unable to fetch location');
     })
 })
